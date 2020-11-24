3685800352faa0b2b7509b6471d5e62cb9168567
SinaKarvandi <ms.karvandi@yahoo.com>
2020-11-24 08:40:11 -0800

    add c# examples

---
+++ C#/Namedpipe/Namedpipe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namedpipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pipe address : \\\\.\\pipe\\HyperDbgTestPipe \nListening...\n");
+            var server = new NamedPipeServerStream("HyperDbgTestPipe");
+            server.WaitForConnection();
+            StreamReader reader = new StreamReader(server);
+            while (true)
+            {
+                var line = reader.ReadLine();
+                Console.Write(line);
+            }
+
+        }
+    }
+}
